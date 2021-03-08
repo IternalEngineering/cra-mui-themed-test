499ac00775495265915f0af7400a9533cd12cd6d
stefankudev-iternal <79448618+stefankudev-iternal@users.noreply.github.com>
2021-03-08 15:59:29 +0000

    make a start on a dark theme (for later)

---
+++ src/IternalThemeDark.js
@@ -0,0 +1,112 @@
+// export const IternalTheme = {
+//     palette: {
+//         common: {
+//             black: '#222',
+//             white: '#fff',
+//         },
+//         type: 'light',
+//         // Calculate with this rough guideline:
+//         // light: +0,-25,+10
+//         // main:  +0, +0,+0
+//         // dark:  +0,+5,-10
+//         primary: {
+//             // L for these values was 30/20/10
+//             light: 'hsl(222, 55%, 45%)',
+//             main: 'hsl(222, 80%, 35%)',
+//             dark: 'hsl(222, 85%, 23%)',
+//             contrastTest: '#ffffff',
+//         },
+//         secondary: {
+//             light: 'hsl(43, 60%, 60%)',
+//             main: 'hsl(43, 85%, 50%)',
+//             dark: 'hsl(43, 90%, 40%)',
+//             contrastTest: '#000000',
+//         },
+//         error: {
+//             light: 'hsl(0, 67%, 43%)',
+//             main: 'hsl(0, 92%, 33%)',
+//             dark: 'hsl(0, 97%, 23%)',
+//             contrastTest: '#ffffff',
+//         },
+//         warning: {
+//             light: 'hsl(24, 59%, 58%)',
+//             main: 'hsl(24, 84%, 48%)',
+//             dark: 'hsl(24, 89%, 38%)',
+//             contrastTest: '#000000',
+//         },
+//         info: {
+//             light: 'hsl(200, 42%, 44%)',
+//             main: 'hsl(200, 67%, 34%)',
+//             dark: 'hsl(200, 72%, 24%)',
+//             contrastTest: '#000000',
+//         },
+//         success: {
+//             light: 'hsl(178, 63%, 46%)',
+//             main: 'hsl(178, 88%, 36%)',
+//             dark: 'hsl(178, 93%, 26%)',
+//             contrastTest: '#000000',
+//         }
+//     },
+//     typography: {
+//         htmlFontSize: 16,
+//         fontSize: 15,
+//         h1: {
+//             fontFamily: `'Maven Pro', 'Helvetica', 'Arial', sans-serif`,
+//             fontWeight: 500,
+//             // fontSize: 44,
+//         },
+//         h2: {
+//             fontFamily: `'Open Sans', 'Helvetica', 'Arial', sans-serif`,
+//             fontWeight: 600,
+//             // fontSize: 36,
+//         },
+//         h3: {
+//             fontFamily: `'Open Sans', 'Helvetica', 'Arial', sans-serif`,
+//             fontWeight: 400,
+//             // fontSize: 28,
+//         },
+//         h4: {
+//             fontFamily: `'Open Sans', 'Helvetica', 'Arial', sans-serif`,
+//             fontWeight: 400,
+//             // fontSize: 24,
+//         },
+//         h5: {
+//             fontFamily: `'Open Sans', 'Helvetica', 'Arial', sans-serif`,
+//             fontWeight: 400,
+//             // fontSize: 22,
+//         },
+//         h6: {
+//             fontFamily: `'Open Sans', 'Helvetica', 'Arial', sans-serif`,
+//             fontWeight: 400,
+//             // fontSize: 20,
+//         },
+//         subtitle1: {
+//             fontFamily: `'Open Sans', 'Helvetica', 'Arial', sans-serif`
+//         },
+//         subtitle2: {
+//             fontFamily: `'Open Sans', 'Helvetica', 'Arial', sans-serif`
+//         },
+//         body1: {
+//             fontFamily: `'Open Sans', 'Helvetica', 'Arial', sans-serif`
+//         },
+//         body2: {
+//             fontFamily: `'Open Sans', 'Helvetica', 'Arial', sans-serif`
+//         },
+//         button: {
+//             fontFamily: `'Open Sans', 'Helvetica', 'Arial', sans-serif`,
+//             letterSpacing: 0.25,
+//             textTransform: 'capitalize',
+//         },
+//         caption: {
+//             fontFamily: `'Open Sans', 'Helvetica', 'Arial', sans-serif`
+//         },
+//         overline: {
+//             fontFamily: `'Open Sans', 'Helvetica', 'Arial', sans-serif`,
+//             fontSize: 14,
+//             letterSpacing: 1.5,
+//         }
+//     },
+//     shape: {
+//         borderRadius: 7
+//     }
+// };
